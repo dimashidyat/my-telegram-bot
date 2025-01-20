9bd994865bdeb3c00bbeecf1504824c9952714c9
dimashidyat <77518929+dimashidyat@users.noreply.github.com>
2025-01-20 14:33:52 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,14 +1,2 @@
-# This file marks the handlers directory as a Python package
-from .pempek import PempekHandler
-from .study import StudyHandler
-from .schedule import ScheduleHandler
-from .health import HealthHandler
-from .relationship import RelationshipHandler
-
-__all__ = [
-    'PempekHandler',
-    'StudyHandler',
-    'ScheduleHandler',
-    'HealthHandler',
-    'RelationshipHandler'
-]
+# Empty __init__.py to mark directory as Python package
+# We'll import the handlers directly in main.py instead
