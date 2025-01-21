c6deb5ad3537d44f5805ff2856c48189f9afd809
dimashidyat <77518929+dimashidyat@users.noreply.github.com>
2025-01-21 07:07:03 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,3 +2,12 @@ from .pempek import PempekHandler
 from .study import StudyHandler
 from .health import HealthHandler
 from .relationship import RelationshipHandler
+from .schedule import ScheduleHandler
+
+__all__ = [
+    'PempekHandler',
+    'StudyHandler',
+    'HealthHandler',
+    'RelationshipHandler',
+    'ScheduleHandler'
+]
